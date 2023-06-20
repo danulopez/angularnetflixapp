545298162f349eb0e5226e2cf548d1b7fc280128
Daniela Lopez <daniela.lopez@bootcamp-upgrade.com>
2023-06-20 14:15:42 +0200

    netflixapp3

--- netflix-app/src/app/app.component.ts
+++ netflix-app/src/app/app.component.ts
@@ -8,6 +8,23 @@ import { Media } from './interfaces/media';
 })
 export class AppComponent {
   title = 'netflix-app';
+public topTenFilms = {
+  section: "Top Ten",
+  films: [{
+    title: "La Casa de Papel",
+    image: "./assets/images/Top10/1-papel.webp"
+    },
+    {
+    title: "La Reina del Flow",
+    image: "./assets/images/Top10/2-reina.webp"
+    },
+    {
+    title: "Titanes",
+    image: "./assets/images/Top10/3-titanes.webp"
+    }
+
+  ]
+}
 public comedyFilms = {
     section: "Comedia",
     films: [{
@@ -27,9 +44,17 @@ public comedyFilms = {
 public actionFilms = {
   section: "Action",
   films: [{
-  title: "Titulo",
-  image: "Imagen"
+    title: "Ejercito de los Muertos",
+    image: "./assets/images/Accion/ejercitomuertos.jpg"
   
+  },{
+    title: "El Protector",
+    image: "./assets/images/Accion/elprotector.webp"
+    
+  },{
+    title: "Equalizer 2",
+    image: "./assets/images/Accion/equalizer2.webp"
+      
   }]
 }
 
@@ -38,10 +63,12 @@ public dramaFilms = {
   films: [{
     title: "1917",
     image: "./assets/images/Drama/1917.webp"
-  },{
+  },
+  {
   title: "arrival",
   image: "./assets/images/Drama/arrival.webp"
-  },{
+  },
+  {
   title: "clublucha",
   image: "./assets/images/Drama/clublucha.webp"
   }]
@@ -50,34 +77,55 @@ public dramaFilms = {
 public animeFilms = {
   section: "Anime",
   films: [{
-  title: "Titulo",
-  image: "Imagen"
-  }]
+  title: "Ajin",
+  image: "./assets/images/Anime/ajin.jpg"
+  },
+  {
+    title: "El Castillo Ambulante",
+    image: "./assets/images/Anime/castilloambulante.webp"
+  },
+  {
+    title: "El Viaje de Chihiro",
+    image: "./assets/images/Anime/chihiro.webp"
+  }
+]
 }
 
 public terrorFilms = {
   section: "Terror",
-  films: [{
-  title: "Titulo",
-  image: "Imagen"
-  }]
+  films: [
+    {
+  title: "El Apóstol",
+  image: "./assets/images/Terror/apostol.jpg"
+  },
+  {
+    title: "La Calle del Terror, parte 2: 1978",
+    image: "./assets/images/Terror/calleterror.jpg"
+  },
+  {
+    title: "Infierno bajo el agua",
+    image: "./assets/images/Terror/infiernoagua.webp"
+  },
+  ]
 }
 
 public scifiFilms = {
   section: "Scifi",
-  films: [{
-  title: "Titulo",
-  image: "Imagen"
-  }]
+  films: [
+  {
+  title: "El 6º día",
+  image: "./assets/images/Sci-fi/6dia.webp"
+  },
+  {
+  title: "12 Monos",
+  image: "./assets/images/Sci-fi/12monos.webp"
+  },
+  {
+  title: "After Earth",
+  image: "./assets/images/Sci-fi/afterearth.webp"
+  }
+]
 }
-public topTenFilms = {
-  section: "Top Ten",
-  films: [{
-    title: "Titulo",
-    image: "Imagen"
-    }
 
-  ]
-}
 }
 
